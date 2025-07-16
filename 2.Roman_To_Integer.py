0b635ed5181f3f2b4a1bcbc0ca90bac8bffd82cb
ambition-spec <ycqin1001@163.com>
2025-07-16 23:26:55 +0800

    更新答案

---
+++ 2.Roman_To_Integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        numeral_map = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, "A":4, "B":9,
+        "E":40, "F":90, "G":400, "H":900}
+        s1 = s.replace("IV","A").replace("IX","B").replace("XL","E").replace("XC","F").replace("CD","G").replace("CM","H")
+        li = list(s1)
+        value = 0
+        for i in li :
+            value += numeral_map[i]
+        return value
+#拿空间换时间，增加了内存但是减少了时间
